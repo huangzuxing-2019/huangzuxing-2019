33c6aacee8b893b8a4c000e5e57a9a2017938151
huangzuxing-2019 <57553773+huangzuxing-2019@users.noreply.github.com>
2020-03-21 13:51:17 +0800

    Add files via upload

---
+++ helloworld/src/com/softeem/demo/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.softeem.demo;//声明包名   单行注释
+
+/*
+ * 多行注释
+ * 第一行
+ * 第二行
+ */
+
+/**
+ * 文档注释
+ * Alt + Shift + j
+ * @author Mr.Zhou
+ *
+ */
+public class HelloWorld {
+	//程序的入口
+	public static void main(String[] args){
+		//println 输出并换行      print 输出不换行
+		System.out.println("HelloWorld!");
+		System.out.println("Hello");
+		System.out.print("world");
+	}
+	
+
+}
